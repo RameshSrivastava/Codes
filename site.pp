16ff73ad0e959710e9443c488b192fd7893d6300
RameshSrivastava <38014741+RameshSrivastava@users.noreply.github.com>
2018-09-08 03:16:22 +0530

    Update site.pp

--- site.pp
+++ site.pp
@@ -7,4 +7,13 @@ service { 'ssh' :
 	ensure => 'running',
 	enable => 'true',
 }
+concat { '/etc/ssh/sshd_config':
+  ensure => present,
+}
+
+concat::fragment { 'sshd_config':
+  target  => '/etc/ssh/sshd_config',
+  content => 'AllowUsers test',
+  order   => '01'
+}
 }
